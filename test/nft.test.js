c7b7a61d4bf5a1482e9211b4d9e3c11627e55be8
Gilles Bruno <gilles_bruno@gmx.fr>
2022-05-15 16:03:02 +0200

    Modify tests due to ownable security

--- test/nft.test.js
+++ test/nft.test.js
@@ -23,21 +23,19 @@ describe("NFTeamWestCoastDapp", async function () {
             //Deploy contracts 
             nft         = await NFT.deploy(nftName, nftSymbol, nftBaseUri);
             marketplace = await Marketplace.deploy(feePercent);
-            console.log('MP address : ' + marketplace.address)
+            //console.log('MP address : ' + marketplace.address)
         }
     )
 
     describe("Deployment", function() {
-        it.skip("Should see correct addresses", async function() {
+        it("Should see correct addresses", async function() {
             expect(`${process.env.PUBLIC_KEY_GANACHE1}`).to.equal(deployer.address)
             //console.log(" deployer : " + deployer.address);
             expect(`${process.env.PUBLIC_KEY_GANACHE2}`).to.equal(addr1.address)
             //console.log(" address 1 : " + addr1.address);
-            expect(`${process.env.PUBLIC_KEY_GANACHE3}`).to.equal(addr2.address)
-            //console.log(" address 2 : " + addr2.address);
         })
         
-        it.skip("Should see correct name and symbol for NFT : "+nftName+" and "+nftSymbol, async function() {
+        it("Should see correct name and symbol for NFT : "+nftName+" and "+nftSymbol, async function() {
             expect(await nft.name()).to.equal(nftName)
             expect(await nft.symbol()).to.equal(nftSymbol)
         })
@@ -47,46 +45,46 @@ describe("NFTeamWestCoastDapp", async function () {
 
     describe("Minting", function() {
 
-        it("Should see correct owner of the minted NFT for address1", async function() {
-            await nft.connect(addr1).mint()
+        it("Should see correct owner for the deployer", async function() {
+            await nft.connect(deployer).mint()
             let newOwner = await nft.ownerOf(1)
-            expect(newOwner).to.equal(addr1.address);
+            expect(newOwner).to.equal(deployer.address);
         })
 
-        it.skip("Should see correct owner of the minted NFT for address2", async function() {
-            await nft.connect(addr2).mint()
+        it("Should see correct owner of the minted NFT for only owner", async function() {
+            await nft.connect(deployer).mint()
             let newOwner = await nft.ownerOf(1)
-            expect(newOwner).to.equal(addr2.address);
+            expect(newOwner).to.equal(deployer.address);
         })
 
-        it.skip("Should see correct balance after minting a NFT for address1", async function() {
-            await nft.connect(addr1).mint()
-            expect(await nft.balanceOf(addr1.address)).to.equal(1);
+        it("Should see correct balance after minting a NFT for only owner", async function() {
+            await nft.connect(deployer).mint()
+            expect(await nft.balanceOf(deployer.address)).to.equal(1);
         })
 
-        it.skip("Should see correct balance after minting a NFT for address2", async function() {
-            await nft.connect(addr2).mint()
-            expect(await nft.balanceOf(addr2.address)).to.equal(1);
+        it("Should see correct balance after minting a NFT for only owner", async function() {
+            await nft.connect(deployer).mint()
+            expect(await nft.balanceOf(deployer.address)).to.equal(1);
         })
 
-        it.skip("Should see correct tokenCount after minting 1 NFT for address1", async function() {
-            await nft.connect(addr1).mint()
+        it("Should see correct tokenCount after minting 1 NFT for only owner", async function() {
+            await nft.connect(deployer).mint()
             expect(await nft.totalSupply()).to.equal(1);
         })
 
-        it.skip("Should see correct tokenCount after minting 1 NFT for address2", async function() {
-            await nft.connect(addr2).mint()
+        it("Should see correct tokenCount after minting 1 NFT for owner (=deployer)", async function() {
+            await nft.connect(deployer).mint()
             expect(await nft.totalSupply()).to.equal(1);
         })
 
-        it.skip("Should see correct tokenCount after minting 2 NFT for address1 and address2", async function() {
-            await nft.connect(addr1).mint()
-            await nft.connect(addr2).mint()
+        it("Should see correct tokenCount after minting 2 NFT for owner", async function() {
+            await nft.connect(deployer).mint()
+            await nft.connect(deployer).mint()
             expect(await nft.totalSupply()).to.equal(2);
         })
 
-        it("Should see correct URI after minting a NFT for address1", async function() {
-            await nft.connect(addr1).mint()
+        it("Should see correct URI after minting a NFT for deployer (only owner)", async function() {
+            await nft.connect(deployer).mint()
             expect(await nft.baseURI()).to.equal(nftBaseUri);
         })
            
@@ -95,15 +93,15 @@ describe("NFTeamWestCoastDapp", async function () {
     describe("Marketplace", function () {
         beforeEach(async function () {
             // addr1 mints an nft
-            await nft.connect(addr1).mint()
+            await nft.connect(deployer).mint()
             // addr1 approves marketplace to spend nft
-            await nft.connect(addr1).setApprovalForAll(marketplace.address, true)
+            await nft.connect(deployer).setApprovalForAll(marketplace.address, true)
           })
 
         it("Should see owner of NFT with tokenid = 1 is the marketplace", async function() {
             // Owner of NFT should now be the marketplace
             let newOwner = await nft.ownerOf(1)
-            expect(newOwner).to.equal(addr1.address);
+            expect(newOwner).to.equal(deployer.address);
         })
   
     })
