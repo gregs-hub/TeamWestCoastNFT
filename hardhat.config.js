ad0867857477b447a9baf3bff53c674afa335f15
Mathieu Bourkel <mathieu.bourkel@gmail.com>
2022-05-15 23:08:41 +0200

    derniere modif avant deploy ropsten2

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,12 +7,7 @@ require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.13",
-  // paths: {
-  //   artifacts: "./artifacts",
-  //   sources: "./contracts",
-  //   cache: "./cache",
-  //   tests: "./test"
-  // },
+
   networks: {
     hardhat: {
       // In this case, a new block will be mined after a random delay of between 3 and 6 seconds.
@@ -21,10 +16,7 @@ module.exports = {
         interval: [3000, 6000],
       },
     },
-	// ganache: {
-	// 	url: "http://127.0.0.1:8545",
-	// 	accounts: [`${process.env.PRIVATE_KEY_GANACHE1}`, `${process.env.PRIVATE_KEY_GANACHE2}`]
-	// },
+
   	ropsten: {
   		url: `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`,
   		accounts: [`${process.env.PRIVATE_KEY}`]
